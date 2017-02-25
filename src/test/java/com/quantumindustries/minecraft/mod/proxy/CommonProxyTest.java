d886342910155cd58b150724f1ffed34660c3556
tanvirt <tanvirt@ufl.edu>
2017-02-25 15:42:48 -0600

    Stubbed proxy tests

---
+++ src/test/java/com/quantumindustries/minecraft/mod/proxy/CommonProxyTest.java
@@ -0,0 +1,18 @@
+package com.quantumindustries.minecraft.mod.proxy;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+class CommonProxyTest {
+
+    @BeforeEach
+    public void setUp() {
+        // TODO
+    }
+
+    @AfterEach
+    public void tearDown() {
+        // TODO
+    }
+
+}
