fd2aee9c1c7645021341bd4962eaad59179dc05c
tanvirt <tanvirt@ufl.edu>
2017-02-26 09:39:11 -0600

    Stubbed tests for mod items

---
+++ src/test/java/com/quantumindustries/minecraft/mod/items/ModItemsTest.java
@@ -0,0 +1,23 @@
+package com.quantumindustries.minecraft.mod.items;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ModItemsTest {
+
+    @BeforeEach
+    public void setUp() {}
+
+    @AfterEach
+    public void tearDown() {}
+
+    @Test
+    public void testInit() {
+        // TODO
+        fail("init() has not been tested");
+    }
+
+}
