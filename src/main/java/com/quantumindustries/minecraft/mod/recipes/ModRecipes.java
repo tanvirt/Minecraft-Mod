08a8f7debc3a47735dd553f6718baf9bf6087604
chris martin <chris martin>
2017-02-28 07:15:32 -0600

    Skeleton for recipes.

---
+++ src/main/java/com/quantumindustries/minecraft/mod/recipes/ModRecipes.java
@@ -0,0 +1,18 @@
+package com.quantumindustries.minecraft.mod.recipes;
+
+
+public class ModRecipes {
+
+    // ---------------------------------------------------------------------
+    // Examples of how to add recipes:
+    //
+    // ItemStack foo = new ItemStack(ModItems.<item>);
+    // GameRegistry.addShapelessRecipe(foo, craft result);
+    // GameRegistry.addShapedRecipe(foo, "xxx", "xxx", "xxx", ModItems.newFoo, 'x');
+    // GameRegistry.addSmelting(<block>, <item result>, <float number exp>);
+    // ---------------------------------------------------------------------
+    public static void init() {
+
+    }
+
+}
