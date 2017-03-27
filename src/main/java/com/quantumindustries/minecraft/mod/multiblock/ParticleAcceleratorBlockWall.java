1441c6473dcc74c7f171177a0de55ae4ec897557
Snyperanihilatr <martinchris6611@gmail.com>
2017-03-26 19:57:40 -0500

    Fixed casing rendering

--- src/main/java/com/quantumindustries/minecraft/mod/multiblock/ParticleAcceleratorBlockWall.java
+++ src/main/java/com/quantumindustries/minecraft/mod/multiblock/ParticleAcceleratorBlockWall.java
@@ -2,6 +2,9 @@ package com.quantumindustries.minecraft.mod.multiblock;
 
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -12,13 +15,27 @@ public class ParticleAcceleratorBlockWall extends ParticleAcceleratorBlockBase {
     }
 
     @SideOnly(Side.CLIENT)
-    public BlockRenderLayer getBlockLayer()
-    {
-        return BlockRenderLayer.CUTOUT_MIPPED;
+    public BlockRenderLayer getBlockLayer() {
+        return BlockRenderLayer.CUTOUT;
     }
 
-    public boolean isFullCube(IBlockState state)
-    {
+    @Override
+    public boolean isFullCube(IBlockState state) {
         return false;
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public boolean isFullyOpaque(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public boolean shouldSideBeRendered(IBlockState blockState, IBlockAccess blockAccess, BlockPos pos, EnumFacing side) {
+        return super.shouldSideBeRendered(blockState, blockAccess, pos, side);
     }
 }
